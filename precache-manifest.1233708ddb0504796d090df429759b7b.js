3395772d9765fbf34a6e5ece54d76a9c8fe5f146
Lena <2863040@ukr.net>
2019-09-28 23:28:29 +0300

    Updates

---
+++ precache-manifest.1233708ddb0504796d090df429759b7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "658da2cade31c34daa65c3256eb2dea5",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "9d6dab4e6de89f268e76",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.64371a6b.chunk.css"
+  },
+  {
+    "revision": "f22a566bc35f2c089453",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.0505ab0d.chunk.js"
+  },
+  {
+    "revision": "9d6dab4e6de89f268e76",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.ddf5b567.chunk.js"
+  },
+  {
+    "revision": "a53a049fe91de8ec0b33",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.e2e23030.js"
+  }
+]);
